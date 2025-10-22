c697421a32500e5f7c28a5d96f7ce2fd0e8167c9
Neco-arch <111620463+Neco-arch@users.noreply.github.com>
2025-10-22 23:38:09 +0700

    Set up npm

---
+++ src/ShipClass.js
@@ -0,0 +1,5 @@
+export class Createship {
+    constructor() {
+
+    }
+}
---
+++ src/ShipClass.test.js
@@ -0,0 +1,7 @@
+import { Createship } from "./ShipClass"
+
+const ClassShip = new Createship()
+
+test('Ship class can display correct information', () => {
+    expect(ClassShip)
+})
