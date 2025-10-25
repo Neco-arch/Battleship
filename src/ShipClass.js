4f0e87a8046a6986c47a33ee11fb1f54cb8fce5e
Neco-arch <111620463+Neco-arch@users.noreply.github.com>
2025-10-25 23:15:33 +0700

    Finish work on Ship class

--- src/ShipClass.js
+++ src/ShipClass.js
@@ -1,5 +1,20 @@
 export class Createship {
-    constructor() {
+    constructor(Name, length, HitsCounter = 0, isSunk = false) {
+        this.Name = Name;
+        this.length = length;
+        this.HitsCounter = HitsCounter;
+        this.isSunk = isSunk;
+    }
 
+    hit() {
+        this.HitsCounter += 1;
+        return
+    }
+
+    isSunk() {
+        if (this.HitsCounter >= this.length) {
+            this.isSunk = true
+        }
+        return
     }
 }
